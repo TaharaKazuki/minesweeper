e6506dc0c7968dbddb46d428c1945d0c6b245042
田原 一樹 <ssp0727.lnc@gmail.com>
2019-09-27 21:26:33 +0900

    [update]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React, { useState, Fragment } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 
 const App = props => {
   const [state, setState] = useState(props)
   const { name, price } = state
+
+  useEffect(() => {
+    console.info('This is Like componentDidMount or componentDidUpdate')
+  })
+
+  useEffect(() => {
+    console.info('This is Like componentDidMount')
+  },[])
+
+  useEffect(() => {
+    console.info('This callback is for name only')
+  },[name])
 
   return (
     <Fragment>
