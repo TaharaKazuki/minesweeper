b96b8570d36014cfc4d55e3f9fc3633c9db89c52
田原 一樹 <ssp0727.lnc@gmail.com>
2021-09-23 19:26:14 +0900

    update

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
-// import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid'
 import { json } from 'express'
 
-dotenv.config({ path: './config.env'})
+dotenv.config({ path: './config.env' })
 
 const app = express()
 
@@ -12,10 +12,42 @@ app.use(cors())
 app.use(json())
 
 let todos = [
-
+  {
+    id: nanoid(),
+    title: 'todo1',
+    completed: true
+  },
+  {
+    id: nanoid(),
+    title: 'todo2',
+    completed: false
+  },
+  {
+    id: nanoid(),
+    title: 'todo3',
+    completed: false
+  },
+  {
+    id: nanoid(),
+    title: 'todo4',
+    completed: false
+  },
+  {
+    id: nanoid(),
+    title: 'todo5',
+    completed: false
+  }
 ]
 
-app.get('/todos', (req, res) => res.send(todos))
+app.get('/todos', (_req, res) => res.send(todos))
+
+app.post('/todos', (req, res) => {
+  const todo = { title: req.body.title, id: nanoid(), completed: false }
+  todos.push(todo)
+  return res.send(todo)
+})
+
+// app.patch('/todos/:id', (req, res) => )
 
 const PORT = 7000
 
