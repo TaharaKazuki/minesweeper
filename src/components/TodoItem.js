6c70d0b6b44337b71bd11704c660fd5bd75287d4
田原 一樹 <ssp0727.lnc@gmail.com>
2021-09-25 00:45:09 +0900

    update

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { toggleComplete } from '../redux/todoSlice'
 
 const TodoItem = ({ id, title, completed }) => {
+  const dispatch = useDispatch()
+
+  const handleCompleteClick = () => {
+    dispatch(
+      toggleComplete({
+        id: id,
+        completed: !completed
+      })
+    )
+  }
+
   return (
     <li
       key={id}
@@ -8,7 +21,12 @@ const TodoItem = ({ id, title, completed }) => {
     >
       <div className="d-flex justify-content-between">
         <span className="d-flex align-items-center">
-          <input type="checkbox" className="mr-3" checked={completed} />
+          <input
+            type="checkbox"
+            className="mr-3"
+            checked={completed}
+            onChange={handleCompleteClick}
+          />
           {title}
         </span>
         <button className="btn btn-danger">Delete</button>
