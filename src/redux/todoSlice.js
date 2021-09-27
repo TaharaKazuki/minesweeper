1012dcdabdf6ecaf5de237c78af10e6f62f5a850
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2021-09-27 10:56:25 +0900

    update

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,4 +1,63 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+export const getTodosAsync = createAsyncThunk(
+  'todos/getTodosAsync',
+  async () => {
+    const response = await fetch('http://localhost:7000/todos')
+    if (response.ok) {
+      const todos = await response.json()
+      return { todos }
+    }
+  }
+)
+
+export const addTodoAsync = createAsyncThunk(
+  'todos/addTodoAsync',
+  async (payload) => {
+    const response = await fetch('http://localhost:7000/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ title: payload.title })
+    })
+
+    if (response.ok) {
+      const todo = await response.json()
+      return { todo }
+    }
+  }
+)
+
+export const toggleCompleteAsync = createAsyncThunk(
+  'todos/completeTodoAsync',
+  async (payload) => {
+    const response = fetch(`http://localhost:7000/todos/${payload.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ completed: payload.completed })
+    })
+    if (response.ok) {
+      const todo = await response.json()
+      return { todo }
+    }
+  }
+)
+
+export const deleteTodoAsync = createAsyncThunk(
+  'todos/deleteTodoAsync',
+  async (payload) => {
+    const response = await fetch(`http://localhost:7000/todos/${payload.id}`, {
+      method: 'DELETE'
+    })
+    if (response.ok) {
+      return { id: payload.id }
+    }
+  }
+)
+
 
 const todoSlice = createSlice({
   name: 'todos',
@@ -21,6 +80,23 @@ const todoSlice = createSlice({
       state[index].completed = action.payload.completed
     },
     deleteTodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload.id)
+    }
+  },
+  extraReducers: {
+    [getTodosAsync.fulfilled]: (_state, action) => {
+      return action.payload.todos
+    },
+    [addTodoAsync.fulfilled]: (state, action) => {
+      state.push(action.payload.todo)
+    },
+    [toggleCompleteAsync.fulfilled]: (state, action) => {
+      const index = state.findIndex(
+        (todo) => todo.id === action.payload.todo.id
+      )
+      state[index].completed = action.payload.todo.completed
+    },
+    [deleteTodoAsync.fulfilled]: (state, action) => {
       return state.filter((todo) => todo.id !== action.payload.id)
     }
   }
