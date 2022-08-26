733aa3dade833fa8140b0ee201d14c7c71247d20
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-08-26 14:55:12 +0900

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,17 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'plugin:prettier/recommended'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-  }
+    quotes: ['error', 'single'],
+  },
+  'comma-dangle': ['error', 'always'],
 }
