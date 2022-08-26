1a5d7004a97e071591f6f07a7516a168490a4911
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-08-26 15:54:01 +0900

    update

--- src/test.ts
+++ src/test.ts
@@ -4,31 +4,42 @@ let c: string
 
 let d: Array<string>
 
-type BasicUser<A = boolean> = {
+type Permission = 'admin' | 'user' | 'manager'
+
+type BasicUser<A = boolean, P = string[]> = {
   name: string
   surname: string
   age: number
   isAdmin: A
+  permission?: P[]
 }
+
+type AdvanceUser = {
+  account: number
+}
+
+type FullUser<A = boolean, P = string[]> = BasicUser<A, P> & AdvanceUser
 
 type UserWithPermissions = {
   permission: string[]
 } & BasicUser
 
-const user: BasicUser<number> = {
+const user: FullUser<boolean, Permission> = {
   name: 'Nick',
   surname: 'Bob',
   age: 30,
-  isAdmin: 1,
+  isAdmin: true,
+  account: 100,
+  permission: ['user'],
 }
 
-let usersArray: BasicUser<number>[] = [user, user, user]
+let usersArray: BasicUser<boolean, Permission>[] = [user, user, user]
 
 const getFirst = <T>(arg: T[]): T => {
   return arg[0]
 }
 
-getFirst<BasicUser<number>>(usersArray)
+getFirst<BasicUser<boolean, Permission>>(usersArray)
 
 type MathFunc = (a: number, b: number) => number
 
